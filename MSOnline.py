28c3f1438fb801708c7c3963ad38b7769556217d
Kevin Lanzinger <87212745+LanzyKevin@users.noreply.github.com>
2024-09-12 08:43:38 -0400

    Update MSOnline.py
    Added keyboard interrupt for exit

--- MSOnline.py
+++ MSOnline.py
@@ -1,6 +1,10 @@
 import pyautogui
 import time
 
-while True:
-  pyautogui.moveRel(0,10)
-  time.sleep(5)
+try:
+    while True:
+        pyautogui.moveRel(0,10)
+        time.sleep(5)
+except KeyboardInterrupt:
+    print("nScript terminated by user.")
+  
